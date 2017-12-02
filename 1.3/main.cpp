48b0fa3513b4745448508fb763b8df8c05974f0d
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-03 00:22:27 +0300

    ok

---
+++ 1.3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    /* Количество футов в миле */
+    const int ftInMile = 5280;
+
+    /* Количество киллометров в миле */
+    const double kmInMile = 1.609344;
+
+    /*
+     * Целочисленные переменные для записи значений
+     * миль, футов, киллометров и метров
+     */
+    int dMile, dFt, dKm, dM;
+
+    
+
+    return 0;
+}
