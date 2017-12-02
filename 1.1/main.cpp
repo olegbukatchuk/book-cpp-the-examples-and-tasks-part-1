ef52bc6f0bfad922c804663bd49bd3a68d63a675
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-02 23:37:40 +0300

    ok

--- 1.1/main.cpp
+++ 1.1/main.cpp
@@ -5,12 +5,8 @@ using namespace std;
 
 int main()
 {
-    system("chcp, utf-8 > nul");
-
     /* Выводим сообщение в консоль */
     cout << "Программируем на C++!" << endl;
-
-    system("pause > nul");
 
     return 0;
 }
---
+++ 1.2/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    /* 1 миля */
+    const double kmInMile = 1.609344;
+
+    /* Расстояние в милях и киллометрах */
+    double distMile, distKm;
+
+    /* Запрос на ввод значения в милях */
+    cout << "Укажите расстояние в милях: ";
+
+    /* Считывание значения для расстояния в милях */
+    cin >> distMile;
+
+    /* Вычисляем расстояние в киллометрах */
+    distKm = distMile * kmInMile;
+
+    /* Выводим результат в консоль */
+    cout << "Расстояние в киллометрах: " << distKm << endl;
+
+    return 0;
+}
