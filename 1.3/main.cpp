5f13925ccf89b0ed58cbea99bfc61aada8b6c5f9
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-03 15:14:09 +0300

    ok

--- 1.3/main.cpp
+++ 1.3/main.cpp
@@ -17,6 +17,30 @@ int main()
      */
     int dMile, dFt, dKm, dM;
 
+    cout << "Расстояние в милях и футах" << endl;
+
+    cout << "Мили: ";
+    cin >> dMile;
+
+    cout << "Футы: ";
+    cin >> dFt;
+
+    /* Считаем расстояние в милях */
+    double distMile = dMile + (double) dFt / ftInMile;
+
+    /* Считаем расстояние в милях */
+    double distKm = distMile * kmInMile;
+
+    /* Киллометры */
+    dKm = (int) distKm;
+
+    /* Метры */
+    dM = (int) ((distKm - dKm) * 1000);
+
+    /* Выводим результаты вычислений в консоль */
+    cout << "Расстояние в киллометрах и метрах." << endl;
+    cout << "Киллометры: " << dKm << endl;
+    cout << "Метры: " << dM << endl;
     
 
     return 0;
