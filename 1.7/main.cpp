945c1bb08311dac639a9aa6d2e9e400ede99d6c7
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-07 21:07:56 +0300

    ok

---
+++ 1.7/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    /* Верхняя грвница суммы */
+    int n, s = 0;
+
+    /* Ввод значения для верхней границы суммы */
+    
+
+    return 0;
+}
