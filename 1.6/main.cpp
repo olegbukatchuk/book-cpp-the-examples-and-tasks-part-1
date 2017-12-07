4309983cf18d92c627fe1749b8715bfad6aceb0f
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-07 20:44:47 +0300

    ok

---
+++ 1.6/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    /* Верхняя граница суммы и значение суммы */
+    int n, s = 0;
+
+    /* Ввод значения для верхней границы ряда */
+    cout << "Верхняя граница суммы";
+
+    
+
+    return 0;
+}
