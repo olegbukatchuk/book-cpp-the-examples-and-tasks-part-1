6c9a023cdacc5c20350786642e54fc5484f07e6f
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-14 15:41:08 +0300

    ok

--- 2.6/main.cpp
+++ 2.6/main.cpp
@@ -1,4 +1,29 @@
-//
-// Created by Oleg Bukatchuk on 12/12/17.
-//
+#include <iostream>
+#include <cstdlib>
 
+using namespace std;
+ int main()
+ {
+     int num, k;
+
+     for (k = 1; k <= 5; k++) {
+         cout << "Укажите число от 1 до 3: ";
+         cin >> num;
+
+         switch (num) {
+             case 1:
+                 cout << "Это единица" << endl;
+                 break;
+             case 2:
+                 cout << "Это двойка" << endl;
+                 break;
+             case 3:
+                 cout << "Это тройка" << endl;
+                 break;
+             default:
+                 cout << "Я не знаю такого числа" << endl;
+         }
+     }
+
+     return 0;
+ }
