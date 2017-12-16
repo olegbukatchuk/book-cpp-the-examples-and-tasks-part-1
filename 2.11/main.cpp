1607280966a2c4e20497f64815548fac41996843
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-16 18:28:43 +0300

    ok

--- 2.11/main.cpp
+++ 2.11/main.cpp
@@ -6,7 +6,28 @@ using namespace std;
 int main()
 {
     cout << "Решение уравнения Ax = B\n";
-    
+
+    double A, B;
+
+    cout << "A = ";
+    cin >> A;
+
+    cout << "B = ";
+    cin >> B;
+
+    try {
+        if (A != 0) {
+            throw A;
+        }
+
+        if (B != 0) {
+            throw "Решений нет";
+        }
+    } catch (double e) {
+        cout << "Решение уравнения: " << B / e << endl;
+    } catch (char* e) {
+        cout << e << endl;
+    }
 
     return 0;
 }
