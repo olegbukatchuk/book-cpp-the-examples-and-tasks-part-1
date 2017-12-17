3e5212478dbd89b98aa693b4ecb9a161e25ab072
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-17 21:37:15 +0300

    ok

---
+++ 3.3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int num;
+
+    int &ref = num;
+    num = 100;
+
+    cout << "num = " << num << endl;
+    cout << "ref = " << ref << endl;
+
+    return 0;
+}
