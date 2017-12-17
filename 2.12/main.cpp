59972e55a21d0a9ac05c91b84de81d941cb51747
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-17 19:57:50 +0300

    ok

--- 2.12/main.cpp
+++ 2.12/main.cpp
@@ -5,7 +5,17 @@ using namespace std;
 
 int main()
 {
-    
+    int n = 10, s = 0, k = 1;
+
+    start:
+    s += k * k;
+
+    if (k < n) {
+        k++;
+        goto start;
+    }
+
+    cout << "Сумма квадратных чисел от 1 до " << n << ": " << s << endl;
 
     return 0;
 }
