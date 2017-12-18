d97be479edb6d35768200e1d1a98e58b40d2316f
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-18 21:34:41 +0300

    ok

--- 4/4.13/main.cpp
+++ 4/4.13/main.cpp
@@ -27,7 +27,31 @@ void show(int* nums, int n)
 
 int main()
 {
-    
+    const int size = 10;
 
+    int numbers[size] = {1, 5, 8, 2, 4, 9, 11, 9, 12, 3};
+
+    show(numbers, size);
+
+    int maxNum = getMax(numbers, size);
+
+    cout << "Максимальное значение: " <<  maxNum << endl;
+
+    maxNum = -100;
+
+    show(numbers, size);
+
+    int &maxRef = getMax(numbers, size);
+
+    cout << "Максимальное значение: " << maxRef << endl;
+
+    maxRef = -200;
+
+    show(numbers, size);
+
+    cout << "Максимальное значение: ";
+
+    cout << getMax(numbers, size) << endl;
+    
     return 0;
 }
