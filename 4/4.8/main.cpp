72442eb0e84e1b6fc08bbd7d4becfe3db09c3f81
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-18 20:14:07 +0300

    ok

--- 4/4.8/main.cpp
+++ 4/4.8/main.cpp
@@ -10,4 +10,17 @@ double mean(double* m, int n)
     for (int k = 0; k < n; k++) {
         s += m[k];
     }
+
+    return s / n;
+}
+
+int main()
+{
+    double A[] = {1, 3, 8, -2, 4};
+    double B[] = {4, 6, 2};
+
+    cout << "Среднее по массиву A: " << mean(A, 5) << endl;
+    cout << "Среднее по массиву B: " << mean(B, 3) << endl;
+
+    return 0;
 }
