0fc00e2f939f9d4a38335b137bab1d09284c883f
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-18 19:10:32 +0300

    ok

---
+++ 4/4.5/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void swap(char a, char b)
+{
+    cout << "Вызывается функция swap()" << endl;
+    cout << "Первый аргумент: " << a << endl;
+    cout << "Второй аргумент: " << b << endl;
+
+    char t = b;
+    b = a;
+    a = t;
+
+    for (int i = 1; i <= 20; i++) {
+        cout << "-";
+    }
+
+    cout << endl;
+
+    cout << "Первый аргумент: " << a << endl;
+    cout << "Второй аргумент: " << b << endl;
+    cout << "Выполнение функции swap() завершено" << endl;
+}
+
+int main()
+{
+    char x = 'A', y = 'B';
+
+    cout << "Первая переменная: " << x << endl;
+    cout << "Вторая переменная: " << y << endl;
+
+    swap(x, y);
+
+    cout << "Первая переменная: " << x << endl;
+    cout << "Вторая переменная: " << y << endl;
+
+    return 0;
+}
