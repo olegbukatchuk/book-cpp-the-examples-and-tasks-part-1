726c32d6d364efd7e9a893902b9e231acda8ce36
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-18 10:16:42 +0300

    ok

---
+++ 3.6/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    srand(2);
+
+    const int width = 9;
+    const int height = 5;
+
+    char Lts[height][width];
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            Lts[i][j] = 'A' + rand() % 25;
+            cout << Lts[i][j] << " ";
+        }
+
+        cout << endl;
+    }
+    
+    return 0;
+}
