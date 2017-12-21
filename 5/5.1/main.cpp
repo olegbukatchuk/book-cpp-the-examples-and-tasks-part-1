0c70190d59bb291f1d3c82a0eb3950772bc5f535
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-21 11:43:01 +0300

    ok

--- 5/5.1/main.cpp
+++ 5/5.1/main.cpp
@@ -15,7 +15,7 @@ public:
     double getMoney()
     {
         double s = money;
-        for (int k = 0; k <= time; k++) {
+        for (int k = 1; k <= time; k++) {
             s *= (1 + rate / 100);
         }
 
@@ -25,13 +25,26 @@ public:
     void showAll()
     {
         cout << "Имя: " << name << endl;
-        
+        cout << "Вклад: " << money << endl;
+        cout << "Ставка (%): " << rate << endl;
+        cout << "Период (лет): " << time << endl;
+        cout << "Итоговая сумма: " << getMoney() << endl;
     }
 };
 
 int main()
 {
+    /* Создание объекта */
+    MyMoney obj;
 
+    /* Присваивание значений полям объекта */
+    obj.name = "Иванов Иван Иванович";
+    obj.money = 1000;
+    obj.rate = 8;
+    obj.time = 5;
+
+    /* Вызов метода */
+    obj.showAll();
 
     return 0;
 }
