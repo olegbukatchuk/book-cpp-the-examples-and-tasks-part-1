68643b2cae40170234c4f044b8d75c62b50f6a24
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-21 22:49:19 +0300

    ok

--- 5/5.6/main.cpp
+++ 5/5.6/main.cpp
@@ -38,4 +38,51 @@ public:
         rate = r;
         time = t;
     }
+
+    MyMoney(string n, double m, double r, int t)
+    {
+        setAll(n, m, r, t);
+    }
+
+    MyMoney()
+    {
+        setAll("", 0, 0, 0);
+    }
+};
+
+class BigMoney: public MyMoney {
+public:
+    int periods;
+
+    double getMoney()
+    {
+        double s = money;
+
+        for (int k = 1; k <= time * periods; k++) {
+            s *= (1 + rate / 100 / periods);
+        }
+
+        return s;
+    }
+
+    void showAll()
+    {
+        cout << "Имя: " << name << endl;
+        cout << "Вклад: " << money << endl;
+        cout << "Ставка (%): " << rate << endl;
+        cout << "Период (лет): " << time << endl;
+        cout << "Начислений в год: " << periods << endl;
+        cout << "Итоговая сумма: " << getMoney() << endl;
+    }
+
+    void setAll(string n, double m, double r, int t)
+    {
+        MyMoney::setAll(n, m, r, t);
+        periods = p;
+    }
+
+    BigMoney(string n, double m, double r, int t, int p = 1)
+    {
+        
+    }
 };
