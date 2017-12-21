c17cf5dabe7e665737748a3453340543bb40a156
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-21 10:37:47 +0300

    ok

--- 5/5.1/main.cpp
+++ 5/5.1/main.cpp
@@ -15,7 +15,17 @@ public:
     double getMoney()
     {
         double s = money;
-        for (int k = 0; )
+        for (int k = 0; k <= time; k++) {
+            s *= (1 + rate / 100);
+        }
+
+        return s;
+    }
+
+    void showAll()
+    {
+        cout << "Имя: " << name << endl;
+        
     }
 };
 
