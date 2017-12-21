44c837659e349f5de68f39e770bd9fc5ba0004b8
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-21 14:30:35 +0300

    ok

--- 5/5.2/main.cpp
+++ 5/5.2/main.cpp
@@ -25,6 +25,32 @@ private:
 public:
     void showAll()
     {
-        
+        cout << "Имя: " << name << endl;
+        cout << "Вклад: " << money << endl;
+        cout << "Ставка (%): " << rate << endl;
+        cout << "Период (лет): " << time << endl;
+        cout << "Итоговая сумма: " << getMoney() << endl;
+    }
+
+    void setAll(string n, double m, double m, double r, int t)
+    {
+        name = n;
+        money = m;
+        rate = r;
+        time = t;
     }
 };
+
+int main()
+{
+    /* Создание объектов */
+    MyMoney objA, objB;
+
+    /* Присваивание значений полям */
+    objA.setAll("Иванов Иван Иванович", 1000, 8, 5);
+    objB.setAll("Петров Пётр Петрович", 1200, 7, 4);
+
+    
+
+    return 0;
+}
