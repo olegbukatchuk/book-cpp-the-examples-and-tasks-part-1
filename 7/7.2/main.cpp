700f87e2982efe188de18833372ef4c19103a08a
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-23 16:59:57 +0300

    ok

--- 7/7.2/main.cpp
+++ 7/7.2/main.cpp
@@ -2,3 +2,12 @@
 #include <cstdlib>
 
 using namespace std;
+
+template <class T> void show(T* m, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << m[i] << " ";
+    }
+
+    cout << endl;
+}
+
