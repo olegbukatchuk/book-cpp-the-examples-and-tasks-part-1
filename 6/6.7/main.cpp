371b48e53bd5b2bb4dffbc69204910d7be81d18c
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-23 14:10:59 +0300

    ok

---
+++ 6/6.7/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class Alpha {
+private:
+    char symb;
+
+public:
+    Alpha(char s) {
+        symb = s;
+    }
+
+    void show() {
+        cout << "Класс Alpha: " << endl;
+    }
+
+    void set(char s) {
+        symb = s;
+    }
+};
+
+class Bravo: public Alpha {
+public:
+    int num;
+
+    Bravo(char s, int n): Alpha(s) {
+        num = n;
+    }
+
+    void showAll() {
+        show();
+        cout << "Класс Bravo: " << num << endl;
+    }
+};
+
+int main() {
+    Bravo obj('A', 100);
+
+    obj.showAll();
+
+    obj.set('Z');
+    obj.showAll();
+
+    return 0;
+}
