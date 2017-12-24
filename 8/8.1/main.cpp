4b1cef1599815f9587108b669a878da9aae8cb94
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-24 16:19:28 +0300

    ok

--- 8/8.1/main.cpp
+++ 8/8.1/main.cpp
@@ -11,3 +11,12 @@ struct MyMoney {
     int time;
 };
 
+double getMoney(MyMoney str) {
+    double s = str.money;
+
+    for (int k = 1; k <= str.time; k++) {
+        s *= (1 + str.rate / 100);
+    }
+
+    return s;
+}
