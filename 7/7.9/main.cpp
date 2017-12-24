30ba6bfb48bf71007b7f109f951ef66284361f26
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-24 14:56:19 +0300

    ok

--- 7/7.9/main.cpp
+++ 7/7.9/main.cpp
@@ -31,5 +31,17 @@ public:
 };
 
 template <class A, class B, class C> class Charlie: public Alpha <A>, public Bravo <B> {
-    
+public:
+    C charlie;
+
+    Charlie(A a, B b, C c): Alpha <A> (a), Bravo <B> (b) {
+        charlie = c;
+    }
+
+    void show() {
+        Alpha <A>::show();
+        Bravo <B>::show();
+
+        cout << "Поле charlie: " << charlie << endl;
+    }
 };
