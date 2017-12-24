b6a9cad5d814fdb8e876792bd02c5ca3f8600bc5
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-24 14:21:34 +0300

    ok

--- 7/7.7/main.cpp
+++ 7/7.7/main.cpp
@@ -3,3 +3,17 @@
 #include <string>
 
 using namespace std;
+
+template <class T> class MyClass {
+private:
+    T value;
+
+    MyClass(T v) {
+        value = v;
+    }
+
+    void show() {
+        cout << "Значение поля: " << value << endl;
+    }
+};
+
