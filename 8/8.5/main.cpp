0c82430279259e9ce05c32e1b6209fcb5df1436a
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-25 17:58:51 +0300

    ok

--- 8/8.5/main.cpp
+++ 8/8.5/main.cpp
@@ -31,4 +31,69 @@ public:
 
         cout << endl;
     }
+
+    double operator() (double x) {
+        valarray <double> b(a);
+        double q = 1;
+
+        for (int k = 0; k < b.size(); k++) {
+            b[k] *= q;
+            q *= x;
+        }
+
+        return b.sum();
+    }
+
+    double &operator[] (int x) {
+        valarray <double> b(a);
+        double q = 1;
+
+        for (int k = 0; k < b.size(); k++) {
+            b[k] *= q;
+            q *= x;
+        }
+
+        return b.sum();
+    }
+
+    double &operator[] (int k) {
+        return a[k];
+    }
+
+    Polynom operator*(Polynom pol) {
+        Polynom tmp(pol.power() + power());
+
+        for (int i = 0; i <= power(); i++) {
+            for (int j = 0; j <= pol.power(); j++) {
+                tmp[i + j] += a[i] * pol[j];
+            }
+        }
+
+        return tmp;
+    }
+
+    Polynom operator+(Polynom pol) {
+        int i;
+        int length = pol.power() > power() ? pol.power() : power();
+
+        Polynom tmp(length);
+
+        for (i = 0; i <= power(); i++) {
+            tmp[i] += a[i];
+        }
+
+        for (i = 0; i <= pol.power(); i++) {
+            tmp[i] += pol[i];
+        }
+
+        return tmp;
+    }
 };
+
+Polynom operator*(Polynom pol, double r) {
+    Polynom tmp(pol.power());
+
+    for (int k = 0; k <= pol.power(); k++) {
+        
+    }
+}
