e1a1c6f7c6a9584a2c7332afd769fbc6bc257c1f
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-25 12:42:31 +0300

    ok

--- 8/8.5/main.cpp
+++ 8/8.5/main.cpp
@@ -4,3 +4,31 @@
 
 using namespace std;
 
+class Polynom {
+private:
+    valarray <double> a;
+
+public:
+    Polynom(int n = 0) {
+        a.resize(n + 1, 0);
+    }
+
+    Polynom(double* nums, int n) {
+        valarray <double> b(nums, n + 1);
+        a = b;
+    }
+
+    int power() {
+        return a.size() - 1;
+    }
+
+    void getAll() {
+        cout << "| ";
+
+        for (int k = 0; k <= power(); k++) {
+            cout << a[k] << " | ";
+        }
+
+        cout << endl;
+    }
+};
