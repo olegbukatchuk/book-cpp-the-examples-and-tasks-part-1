0a0eefaf2acf29fd6f41b779a55995d98a27b259
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-27 10:35:13 +0300

    ok

--- 8/8.6/main.cpp
+++ 8/8.6/main.cpp
@@ -3,3 +3,25 @@
 #include <vector>
 
 using namespace std;
+
+int main() {
+    srand(2);
+
+    int n;
+
+    cout << "Количество символов: ";
+    cin >> n;
+
+    vector <char> symbs(n);
+    cout << "|";
+
+    for (int k = 0; k < symbs.size(); k++) {
+        symbs[k] = 'A' + rand() % (n + 5);
+
+        cout << " " << symbs[k] << " |";
+    }
+
+    cout << endl;
+
+    return 0;
+}
