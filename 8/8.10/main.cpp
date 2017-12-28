7070b0e459d1af6ff45429d79588237100d46add
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-28 21:01:12 +0300

    ok

---
+++ 8/8.10/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <map>
+#include <utility>
+
+using namespace std;
+
+int main() {
+    map<string,int> numbers;
+
+    const int n=5;
+
+    string names[n]={"один","два","три","четыре","пять"};
+
+    int nms[n]={1,2,3,4,5};
+
+    for (int k=0;k<n;k++) {
+        numbers.insert(pair<string, int>(names[k], nums[k]));
+    }
+
+    numbers.erase("три");
+
+    return 0;
+}
