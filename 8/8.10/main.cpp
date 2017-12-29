ecf5c0ce2db184230465e95717ad84ae8811843e
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-29 12:15:32 +0300

    ok

--- 8/8.10/main.cpp
+++ 8/8.10/main.cpp
@@ -16,12 +16,22 @@ int main() {
     int nms[n]={1,2,3,4,5};
 
     for (int k=0;k<n;k++) {
-        numbers.insert(pair<string, int>(names[k], nums[k]));
+        numbers.insert(pair<string, int>(names[k], nms[k]));
     }
 
     numbers.erase("три");
 
     map<string,int>::iterator p;
+
+    p=numbers.begin();
+
+    while(p!=numbers.end()) {
+        cout<<(*p).first<<"\t- "<<(*p).second<<endl;
+        p++;
+    }
+
+    cout<<"Единица\t - это "<<numbers["один"]<<endl;
+    cout<<"Двойка\t - это "<<numbers.at("два")<<endl;
 
     return 0;
 }
