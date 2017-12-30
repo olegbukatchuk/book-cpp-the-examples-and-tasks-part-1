75d1da8d4bf3dde4c8654e0e86b03b5ca35bba19
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-30 23:37:58 +0300

    ok

--- 9/9.7/main.cpp
+++ 9/9.7/main.cpp
@@ -5,5 +5,27 @@
 using namespace std;
 
 double integrate(double (*f)(double), double a, double b, double Fmax) {
-    
+    double x,y;
+
+    int m=10000;
+
+    double dx=(b-a)/m;
+    double dy=Fmax/m;
+
+    int count=0;
+
+    for (int i=0;i<=m;i++) {
+        for (int j=0lj<=m;j++) {
+            x=a+i*dx;
+
+            if (y<=f(x)) {
+                count++;
+            }
+        }
+    }
+
+    double z=(double)count/(m+1)/(m+1);
+
+    return Fmax*(b-a)*z;
 }
+
