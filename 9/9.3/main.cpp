8ab3f6e3e670b1c09277d599daed9b1cd749f41e
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-30 22:40:39 +0300

    ok

--- 9/9.3/main.cpp
+++ 9/9.3/main.cpp
@@ -8,3 +8,18 @@ double f(double x) {
     return 2*exp(-x)-1;
 }
 
+int main() {
+    int n=10;
+
+    double dx=0.00001;
+    double x=0;
+
+    for (int k=1;k<=n;k++) {
+        x=x-f(x)/((f(x+dx)-f(x))/dx);
+    }
+
+    cout<<"Найдено такое решение:\t"<<x<<endl;
+    cout<<"Контрольное значение:\t"<<log(2)<<endl;
+
+    return 0;
+}
