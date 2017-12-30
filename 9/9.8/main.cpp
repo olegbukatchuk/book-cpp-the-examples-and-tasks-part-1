d1c795481faa55123e7dac718a91754d010f6302
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-30 23:48:30 +0300

    ok

--- 9/9.8/main.cpp
+++ 9/9.8/main.cpp
@@ -8,3 +8,15 @@ double f(double x, double y) {
     return x*x*exp(-x)-y;
 }
 
+double dsolve(double (*f)(double, double), double x0, double y0, double x) {
+    int n=1000;
+
+    double h=(x-x0)/n;
+    double y=y0;
+
+    for (int k=0;k<n;k++) {
+        y=y+h*f(x0+k*h,y);
+    }
+    return y;
+}
+
