c3041284026223739ebfb53721e5c32eec72b3fa
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-30 22:57:30 +0300

    ok

--- 9/9.5/main.cpp
+++ 9/9.5/main.cpp
@@ -3,3 +3,14 @@
 
 using namespace std;
 
+void findA(double* a, double* x, double* y, int n) {
+    double q;
+    a[0]=y[0];
+
+    for (int k=1;k<n;k++) {
+        a[k]=y[k];
+        q=1;
+
+        for ()
+    }
+}
