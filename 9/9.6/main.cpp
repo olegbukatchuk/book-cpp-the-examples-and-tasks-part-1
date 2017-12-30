509da54d0c6cc085d970435ef6f96a32ca713f2c
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-30 23:18:22 +0300

    ok

--- 9/9.6/main.cpp
+++ 9/9.6/main.cpp
@@ -4,5 +4,10 @@
 
 using namespace std;
 double integrate(double (*f)(double), double a, double b, int m=1000) {
-    
+    double h=(b-a)/2/m;
+    double s=0;
+
+    for (int k=1;k<=m-1;k++) {
+        
+    }
 }
