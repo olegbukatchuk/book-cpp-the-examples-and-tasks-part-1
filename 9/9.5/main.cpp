9bf22e21c9d4e959dc4e48ad0da77afe1a3b5b42
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-12-30 23:03:00 +0300

    ok

--- 9/9.5/main.cpp
+++ 9/9.5/main.cpp
@@ -11,6 +11,39 @@ void findA(double* a, double* x, double* y, int n) {
         a[k]=y[k];
         q=1;
 
-        for ()
+        for (int m=0;m<k;m++) {
+            a[k]-=a[m]*q;
+            q*=(x[k]-x[m]);
+        }
+
+        a[k]/=q;
     }
+}
+
+double P(double* a, double z, double* x, int n) {
+    double s=0;
+    double q;
+
+    for (int k=0;k<n;k++) {
+        q=1;
+
+        for (int m=0;m<k;m++) {
+            q*=(z-x[m]);
+        }
+        s+=a[k]*q;
+    }
+    return s;
+}
+
+void line(int m) {
+    for (int k=1;k<=m;k++) {
+        cout<<"-";
+    }
+    cout<<endl;
+}
+
+int main() {
+    
+
+    return 0;
 }
